8ba374c07e05b06b5cd4e5423164c5aaa5d61abf
VladBrok <vlad.brok99@gmail.com>
2023-05-23 10:22:36 +0300

    add lazy loading in userCard

--- src/features/user/userCard.tsx
+++ src/features/user/userCard.tsx
@@ -1,14 +1,26 @@
 import "./userCard.css"
 import Container from "react-bootstrap/Container"
-import Card from "react-bootstrap/Card"
 import PostList from "../posts/postList"
-import { useEffect } from "react"
+import { Suspense, lazy, useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { getUser } from "./userSlice"
-import Avatar from "../avatar/avatar"
-import ListGroup from "react-bootstrap/ListGroup"
-import Alert from "react-bootstrap/Alert"
-import Spinner from "react-bootstrap/Spinner"
+
+const Spinner = lazy(() => import("react-bootstrap/Spinner"))
+const ListGroup = lazy(() => import("react-bootstrap/ListGroup"))
+const ListGroupItem = lazy(() => import("react-bootstrap/ListGroupItem"))
+const Alert = lazy(() => import("react-bootstrap/Alert"))
+const Card = lazy(() => import("react-bootstrap/Card"))
+const CardTitle = lazy(() =>
+  import("react-bootstrap/Card").then((module) => ({
+    default: module.default.Title,
+  })),
+)
+const CardBody = lazy(() =>
+  import("react-bootstrap/Card").then((module) => ({
+    default: module.default.Body,
+  })),
+)
+const Avatar = lazy(() => import("../avatar/avatar"))
 
 export interface IUserCardProps {
   userId: number
@@ -26,43 +38,45 @@ export default function UserCard(props: IUserCardProps) {
 
   return (
     <Container fluid className="d-flex p-0 mt-5 pt-2 user-card__container">
-      <div className="border-end user-card__info">
-        {isLoading && (
-          <div className="d-flex justify-center mt-5">
-            {" "}
-            <Spinner animation="border" className="mx-auto" />
-          </div>
-        )}
-        {isError && (
-          <Alert variant="danger" className="mt-5 mx-3">
-            При загрузке информации о пользователе произошла ошибка. Попробуйте
-            перезагрузить страницу.
-          </Alert>
-        )}
-        {!isLoading && !isError && (
-          <Card className="border-end-0 border-start-0">
-            <div className="px-4 d-flex justify-content-center">
-              <Avatar className="user-card__avatar mt-4 border-secondary border-2 rounded-circle" />
+      <Suspense>
+        <div className="border-end user-card__info">
+          {isLoading && (
+            <div className="d-flex justify-center mt-5">
+              {" "}
+              <Spinner animation="border" className="mx-auto" />
             </div>
-            <Card.Body>
-              <Card.Title className="text-center">
-                {user.info?.name} ({user.info?.username})
-              </Card.Title>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroup.Item>
-                email: <span className="fw-bold"> {user.info?.email}</span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                phone: <span className="fw-bold">{user.info?.phone}</span>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                website: <span className="fw-bold">{user.info?.website}</span>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        )}
-      </div>
+          )}
+          {isError && (
+            <Alert variant="danger" className="mt-5 mx-3">
+              При загрузке информации о пользователе произошла ошибка.
+              Попробуйте перезагрузить страницу.
+            </Alert>
+          )}
+          {!isLoading && !isError && (
+            <Card className="border-end-0 border-start-0">
+              <div className="px-4 d-flex justify-content-center">
+                <Avatar className="user-card__avatar mt-4 border-secondary border-2 rounded-circle" />
+              </div>
+              <CardBody>
+                <CardTitle className="text-center">
+                  {user.info?.name} ({user.info?.username})
+                </CardTitle>
+              </CardBody>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>
+                  email: <span className="fw-bold"> {user.info?.email}</span>
+                </ListGroupItem>
+                <ListGroupItem>
+                  phone: <span className="fw-bold">{user.info?.phone}</span>
+                </ListGroupItem>
+                <ListGroupItem>
+                  website: <span className="fw-bold">{user.info?.website}</span>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          )}
+        </div>
+      </Suspense>
       <div className="user-card__posts">
         <PostList userId={props.userId} />
       </div>
