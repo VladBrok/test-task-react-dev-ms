abc5c74871855cbb4e4b8d89d98ff2a40fa2289a
VladBrok <vlad.brok99@gmail.com>
2023-05-23 08:58:37 +0300

    display user info on the user card

--- src/features/user/userCard.tsx
+++ src/features/user/userCard.tsx
@@ -1,8 +1,11 @@
 import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
 import PostList from "../posts/postList"
 import { useEffect } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { getUser } from "./userSlice"
+import Avatar from "../avatar/avatar"
+import ListGroup from "react-bootstrap/ListGroup"
 
 export interface IUserCardProps {
   userId: number
@@ -28,7 +31,25 @@ export default function UserCard(props: IUserCardProps) {
       style={{ height: "calc(100vh - 48px)", overflow: "hidden" }}
     >
       <div style={{ flex: "1 1 30%" }} className="border-end">
-        user # {props.userId}
+        <Card className="border-end-0 border-start-0">
+          <Avatar className="border-secondary border-2 rounded-circle m-4 mb-0" />
+          <Card.Body>
+            <Card.Title className="text-center">
+              {user.info?.name} ({user.info?.username})
+            </Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              email: <span className="fw-bold"> {user.info?.email}</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              phone: <span className="fw-bold">{user.info?.phone}</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              website: <span className="fw-bold">{user.info?.website}</span>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
       </div>
       <div style={{ overflowY: "auto", flex: "1 1 70%" }}>
         <PostList userId={props.userId} />
