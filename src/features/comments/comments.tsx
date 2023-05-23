c5c8ebb11cda27d53b1e5f2421431045efb671aa
VladBrok <vlad.brok99@gmail.com>
2023-05-23 10:00:40 +0300

    add lazy loading in comments

--- src/features/comments/comments.tsx
+++ src/features/comments/comments.tsx
@@ -1,13 +1,9 @@
 import "./comments.css"
-import ListGroup from "react-bootstrap/ListGroup"
 import { useAccordionButton } from "react-bootstrap/AccordionButton"
 import Button from "react-bootstrap/Button"
-import Alert from "react-bootstrap/Alert"
 import Container from "react-bootstrap/Container"
-import Spinner from "react-bootstrap/Spinner"
 import Accordion from "react-bootstrap/Accordion"
-import { useEffect, useState } from "react"
-import { Card } from "react-bootstrap"
+import { Suspense, lazy, useEffect, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import {
   areCommentsLoading,
@@ -17,6 +13,15 @@ import {
 } from "./commentsSlice"
 import { shallowEqual } from "react-redux"
 
+const Spinner = lazy(() => import("react-bootstrap/Spinner"))
+const Alert = lazy(() => import("react-bootstrap/Alert"))
+const ListGroup = lazy(() => import("react-bootstrap/ListGroup"))
+const ListGroupItem = lazy(() => import("react-bootstrap/ListGroupItem"))
+const Card = lazy(() => import("react-bootstrap/Card"))
+const CardText = lazy(() =>
+  import("react-bootstrap/Card").then((x) => ({ default: x.default.Text })),
+)
+
 export interface ICommentsProps {
   postId: number
 }
@@ -25,6 +30,8 @@ const ACCORDION_KEY = "0"
 
 export default function Comments(props: ICommentsProps) {
   const [activeKey, setActiveKey] = useState("")
+
+  const isOpen = Boolean(activeKey)
 
   const comments = useAppSelector(
     selectCommentsByPostId(props.postId),
@@ -39,53 +46,57 @@ export default function Comments(props: ICommentsProps) {
   })
 
   useEffect(() => {
-    if (!activeKey) {
+    if (!isOpen) {
       return
     }
 
     dispatch(getComments(props.postId))
-  }, [activeKey, dispatch, props.postId])
+  }, [dispatch, isOpen, props.postId])
 
   return (
     <Accordion activeKey={activeKey}>
       <Button onClick={decoratedOnClick}>Комментарии</Button>
       <Accordion.Collapse eventKey={ACCORDION_KEY}>
-        <Container fluid className="mt-3">
-          {isLoading && (
-            <div className="d-flex justify-center">
-              {" "}
-              <Spinner animation="border" className="mx-auto" />
-            </div>
-          )}
-          {isError && (
-            <Alert variant="danger">
-              При загрузке комментариев произошла ошибка. Попробуйте позже.
-            </Alert>
-          )}
-          {!isLoading &&
-            !isError &&
-            (comments.length ? (
-              <ListGroup className="comments__list">
-                {comments.map((comment) => (
-                  <ListGroup.Item
-                    className="border-start-0 border-end-0"
-                    key={comment.id}
-                  >
-                    <Card className="border-0">
-                      <Card.Text className="fw-bold mb-1">
-                        {comment.email}
-                      </Card.Text>
-                      <Card.Text>{comment.body}</Card.Text>
-                    </Card>
-                  </ListGroup.Item>
+        <Suspense>
+          {isOpen && (
+            <Container fluid className="mt-3">
+              {isLoading && (
+                <div className="d-flex justify-center">
+                  {" "}
+                  <Spinner animation="border" className="mx-auto" />
+                </div>
+              )}
+              {isError && (
+                <Alert variant="danger">
+                  При загрузке комментариев произошла ошибка. Попробуйте позже.
+                </Alert>
+              )}
+              {!isLoading &&
+                !isError &&
+                (comments.length ? (
+                  <ListGroup className="comments__list">
+                    {comments.map((comment) => (
+                      <ListGroupItem
+                        className="border-start-0 border-end-0"
+                        key={comment.id}
+                      >
+                        <Card className="border-0">
+                          <CardText className="fw-bold mb-1">
+                            {comment.email}
+                          </CardText>
+                          <CardText>{comment.body}</CardText>
+                        </Card>
+                      </ListGroupItem>
+                    ))}
+                  </ListGroup>
+                ) : (
+                  <Alert variant="secondary">
+                    У данного поста нет комментариев.
+                  </Alert>
                 ))}
-              </ListGroup>
-            ) : (
-              <Alert variant="secondary">
-                У данного поста нет комментариев.
-              </Alert>
-            ))}
-        </Container>
+            </Container>
+          )}
+        </Suspense>
       </Accordion.Collapse>
     </Accordion>
   )
